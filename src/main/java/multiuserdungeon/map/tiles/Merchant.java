7daf0ea5f4ba5f855084e8a0a0f5d2f8d5720670
xXSpaghett69Xx <qjm412@gmail.com>
2024-11-12 08:53:04 -0500

    Merchant should be ready. The actual exchange of items and currency to be handled by a higher up class.

---
+++ src/main/java/multiuserdungeon/map/tiles/Merchant.java
@@ -0,0 +1,59 @@
+package multiuserdungeon.map.tiles;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+import multiuserdungeon.map.Tile;
+import multiuserdungeon.inventory.*;
+import multiuserdungeon.*;
+import multiuserdungeon.clock.*;
+
+
+public class Merchant {
+    private String name;
+    private Map<InventoryElement, Integer> store;
+    private Tile tile;
+
+    public Merchant (String name, List<InventoryElement> contents) {
+        this.name = name;
+        this.store = new HashMap<>();
+        for (InventoryElement item: contents) {
+            store.put(item,item.getGoldValue()*2);
+            if (store.size() > 3) {
+                break;
+            }
+        }
+        this.tile = null;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+    public boolean passable() {
+        return false;
+    }
+    public char getASCII() {
+        return 'M';
+    }
+    public Map<InventoryElement, Integer> getStore() {
+        return this.store;
+    }
+    public int buyItem (InventoryElement item) {
+        return item.getGoldValue() / 2;
+    }
+    public InventoryElement handleSale(InventoryElement item) {
+        store.remove(item);
+        return item;
+    }
+    public boolean isOpen() {
+        return Game.getInstance().getMap().getPlayerRoom().isSafe() && Game.getInstance().getCurrentTime() instanceof Day;
+    }
+}
