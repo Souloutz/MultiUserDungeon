3871cdf8d1340150f3e1482f04fe10a7f5b81205
Howard Kong <howardkong826@gmail.com>
2024-11-14 15:11:19 -0500

    Update command to match updated parameters for updated password changing

--- src/main/java/multiuserdungeon/commands/authentication/ChangePasswordAction.java
+++ src/main/java/multiuserdungeon/commands/authentication/ChangePasswordAction.java
@@ -8,17 +8,19 @@ public class ChangePasswordAction implements Action<Boolean> {
     private final Authenticator receiver;
     private final String curPassword;
     private final String newPassword;
+    private final String confirmPassword;
 
-    public ChangePasswordAction(Authenticator authenticator, String curPassword, String newPassword) {
+    public ChangePasswordAction(Authenticator authenticator, String curPassword, String newPassword, String confirmPassword) {
         this.receiver = authenticator;
         this.curPassword = curPassword;
         this.newPassword = newPassword;
+        this.confirmPassword = confirmPassword;
     }
 
     @Override
     public Boolean execute() {
         if (canExecute())
-           return this.receiver.handleChangePassword(curPassword, newPassword);
+           return this.receiver.handleChangePassword(curPassword, newPassword, confirmPassword);
 
         return false;
     }
